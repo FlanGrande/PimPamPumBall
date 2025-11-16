c595bb43cb34fbdd6e183775c84826728e4dca12
FlanGrande <francisjgrande@gmail.com>
2025-11-16 23:57:28 +0000

    Polished bouncer

--- nim/PimPamPumBall/src/classes/bouncer.nim
+++ nim/PimPamPumBall/src/classes/bouncer.nim
@@ -19,24 +19,25 @@ type Bouncer* {.gdsync.} = ptr object of StaticBody2D
 method ready(self: Bouncer) {.gdsync.} =
   discard self.bouncy_part_area_2d.connect("body_entered", self.callable("_on_body_entered"))
   
-  if self.scale.x == -1.0:
-    self.side = Side.RIGHT
+  if self.side == Side.RIGHT:
+    self.setScale(vector2(-1.0, 1.0))
 
 
 method process(self: Bouncer; delta: float64) {.gdsync.} =
   discard
 
-
+# Not terribly flexible for RIGHT side, but if I used it in LEFT mode with a symmetrical sprite, I think it can be on any position
+# Which begs the question: do I really need the Side feature? XD
 proc ball_collided(self: Bouncer, body: Node2D) {.gdsync, name: "_on_body_entered".} =
   if body.isInGroup("balls"):
     let ball: RigidBody2D = body as RigidBody2D
 
     if ball != nil:
       let collisionShape2d = self.bouncy_part_area_2d.getChildren()[0] as CollisionShape2D
-      var impulse_direction = vector2(0.0, -1.0).rotated(collisionShape2d.rotation + PI/2)
+      var impulse_direction: Vector2 = vector2(0.0, -1.0).rotated(self.globalRotation + collisionShape2d.rotation + PI/2)
 
       if self.side == Side.RIGHT:
-        impulse_direction = impulse_direction.rotated(collisionShape2d.rotation - PI/2)
+        impulse_direction = impulse_direction.reflect(vector2(1.0, 0.0))
 
       print(impulse_direction)
       
